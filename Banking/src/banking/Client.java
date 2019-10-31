4c5872152f475270281b28bbd8752cdeefe9b3b4
Nick <36056036+nickuniverse91@users.noreply.github.com>
2019-10-31 11:16:32 -0400

    Add files via upload
    v2

--- Banking/src/banking/Client.java
+++ Banking/src/banking/Client.java
@@ -19,6 +19,35 @@ public class Client implements IClient{
     private String firstName;
     private String lastName;
     ArrayList<Account>accountList = new ArrayList<Account>();
+
+    public Client(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
     
     
     
