f2527fe115c496772b0191e690732af289ce4d63
BloopersPrat <57198567+BloopersPrat@users.noreply.github.com>
2019-11-06 17:34:55 -0500

    Add files via upload

--- Banking/src/banking/UserInputManager.java
+++ Banking/src/banking/UserInputManager.java
@@ -5,10 +5,77 @@
  */
 package banking;
 
-/**
- *
- * @author cstuser
- */
+import java.util.Scanner;
+
+
 public class UserInputManager implements IUserInputManager{
+    
+    private static String Scanning(){
+        Scanner scan = new Scanner(System.in);
+        String Input = scan.nextLine();
+        return Input;
+    }
+
+    public int retrieveAccountNumber(){
+        System.out.println("Please enter account number: ");
+        int AccNum = Integer.parseInt(Scanning());
+        return AccNum;
+    }
+    
+    public Account retrieveAccountType(){
+        System.out.println("Please input your desired account type");
+        System.out.println("Type Checking for a CheckingAccount or Type Saving for a SavingsAccount: ");
+        
+        String AccountType = Scanning();
+            if (AccountType == "Checking"){
+            }
+                else if (AccountType == "Saving" ) {
+                }
+            System.out.println(AccountType);
+    return null; //to fix
+        
+    }
+    public int retrieveClientId(){
+        System.out.println("Please input your Id: ");
+        int Id = Integer.parseInt(Scanning());
+        return Id;
+    }
+    
+    public Client retrieveClientInfo(){ 
+        System.out.println("Please enter your First Name: ");
+        String FirstName = Scanning();
+        System.out.println("Please enter your Last Name: ");
+        String LastName = Scanning();
+        System.out.println(FirstName +" " + LastName);
+        
+        return null; //to fix
+    }
+    
+    public double retrieveTransactionAmount(){
+        String Transaction = Scanning();
+        if(Transaction == "Deposit"){
+            System.out.println("Please input your desired amount to deposit: ");
+            double Deposit = Double.parseDouble(Scanning());
+            return Deposit + /* value already in the bank*/;  
+        }
+        else if(Transaction == "Withdraw"){
+            System.out.println("Please input your desired amount to withdraw: ");
+            double Deposit = Double.parseDouble(Scanning());
+            return Deposit - /* value already in the bank*/;  
+        }
+        return 0;//to fix
+    }
+    
+    public int retrieveUserOption(){
+        System.out.println("Please select one of the following numbers: ");
+        int entry = Integer.parseInt(Scanning());
+        return entry;
+    }
+
+    
+    
+    
+    
+    
     
 }
