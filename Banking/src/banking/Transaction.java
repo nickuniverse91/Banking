9c7db6a73abcc5ad9818a391497a718cb2c03b43
BloopersPrat <57198567+BloopersPrat@users.noreply.github.com>
2019-11-14 10:20:06 -0500

    Add files via upload

--- Banking/src/banking/Transaction.java
+++ Banking/src/banking/Transaction.java
@@ -13,5 +13,33 @@ public class Transaction implements ITransaction{
     
     private String type;
     private double amount;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    public String toString(){
+        return this.type + " of " + this.amount + "$";
+    }
+
     
 }
+
+
+
